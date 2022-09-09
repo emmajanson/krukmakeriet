ae7884ed80da1c089d0773730ddf5b9f51e28ddc
Philip Rydqvist <philip.rydqvist@gmail.com>
2022-09-09 10:40:26 +0200

    lagt in allt i komponent users

--- src/App.js
+++ src/App.js
@@ -11,39 +11,11 @@ import styles from "./App.module.css";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 
-import UsersFB from "./Components/Users";
-
-//för inlogg/auth
-import { useState, useEffect } from "react";
-import {db} from "./firebase-config.js"
-import { collection, getDocs, addDoc } from "firebase/firestore"; 
-
 
 //Lägg in nestlade routes för att hantera kategorier i webshopen & adminsidan
 
 function App() {
-  
-  //för inlogg/auth
-  /* const [newName, setNewName] = useState("")
-  const [newEmail, setNewEmail] = useState("")
-  const [newPwd, setNewPwd] = useState("")
-
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
-  
-   const createUser = async () => {
-    await addDoc(usersCollectionRef, {name: newName, email: newEmail, password: newPwd});
-   }
-
-  useEffect (() => {
-    const getUsers = async () => {
-    const data = await getDocs(usersCollectionRef);
-     /* console.log(data); */ 
-    /* setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
-    };
-
-    getUsers();
-  }, []);  */ 
+   
 
   return (
     <div className={styles.wrapper}>
@@ -59,41 +31,10 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-        <Footer />
+        <Footer/>
       </Router>
       
-      <input 
-      type="text" 
-      name="name" 
-      placeholder="Enter your name..." 
-      onChange={(event) => {
-      setNewName(event.target.value);
-      }}
-      />
-       <input 
-      type="email" 
-      email="email"
-      placeholder="Enter your email..." 
-      onChange={(event) => {
-        setNewEmail(event.target.value);
-        }}
-      />
-      <input 
-      type="password" 
-      name="password" 
-      placeholder="Enter your password..." 
-      onChange={(event) => {
-        setNewPwd(event.target.value);
-        }}
-      />
-      
-      <button onClick={createUser}> Create User </button>
-      {users.map((user) => {
-        return <div> <h1>Name: {user.name}</h1>
-        <h1>Email: {user.email}</h1>
-        <h1>Password: {user.password}</h1>
-        </div>;
-      })}
+     
     </div>
   );
 }
