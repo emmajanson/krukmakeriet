ffee3f5e6414f96a4b05fac3d3298cdde2308dc1
Albin Gerhardsson <albingerhardsson123@gmail.com>
2022-09-12 14:20:37 +0200

    Jobbat med kurser samt börjat på localstorage

--- src/Components/CourseItem.js
+++ src/Components/CourseItem.js
@@ -1,35 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./CourseItem.module.css";
 
 //Ska finnas med
 //OnClick på knappen för att boka
 
-function CourseItem(props) {
- 
+function CourseItem({courseData}) {
+
+  //state som är vår varukorg
+  const [basket, setBasket] = useState([])
+
+  function addCourseToBasket (courseData) {
+    console.log(courseData)
+
+    setBasket(prevBasket => prevBasket + courseData)
+
+    
+    //klickad kurs lagras i vårt state
+    //vi uppdaterar vår LS med vårt state
+    localStorage.setItem('courseBasket', JSON.stringify(basket))
+  }
+  
+  
+
   return (
     <article className={styles.courseCardWrapper}>
       <div className={styles.imgWrapper}>
-        <img className={styles.courseImage} src={props.props.imgUrl} alt="" />
+        <img className={styles.courseImage} src={courseData.img} alt="" />
       </div>
-
+      
       <div className={styles.textWrapper}>
 
         <div className={styles.infoWrapper}>
-          <h3 className={styles.name}>{props.props.name}</h3>
-          <p className={styles.date}>[ikon]{props.props.date}</p> 
-          <p className={styles.info}>{props.props.info}</p>
+          <h3 className={styles.name}>{courseData.name}</h3>
+          <p className={styles.date}>[ikon]{courseData.details}</p> 
+          <p className={styles.info}>{courseData.info}</p>
         </div>
 
-        
         <div className={styles.iconWrapper}>
-          <p className={styles.price}>[ikon] {props.props.price}:-</p>
-          <p className={styles.length}>[ikon] {props.props.duration}</p>
-          <p className={styles.spots}>[ikon] [10/12]</p>
+          <p className={styles.price}>[ikon] {courseData.price}:-</p>
+          <p className={styles.length}>[ikon] {courseData.duration}</p>
+          <p className={styles.spots}>[ikon] [10/{courseData.spots}]</p>
         </div>
 
-        <button className={styles.button}>Boka</button>
+        <button className={styles.button} onClick={() => addCourseToBasket(courseData)}>Boka</button>
       </div>
-      
+    
     </article>
   )
 }
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,35 +1,85 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import CourseItem from "../Components/CourseItem";
 import styles from "./Courses.module.css";
 import Events from "../Components/Events";
+import { db } from "../firebase-config";
+import {
+  collection,
+  getDocs
+} from "firebase/firestore";
 
-// Det här ska finnas
-// - fetcha kurserna från db
-// - sortera efter datum
-// - funktion för att mappa ut alla kurser som finns
+// DET HÄR SKA FINNAS
 // - funktion för att lägga kurs i varukorgen
 
 //nedan: endast testobjekt
-const testCourse = {
-  date: "xx/xx/xx xx:xx",
-  duration: "xx:xx",
-  info: "Info om kursen. Info om kursen. Info om kursen. Info om kursen. Info om kursen",
-  name: "Kursnamn",
-  price: 500,
-  spots: 10,
-  imgUrl: "",
-};
+// const testCourses = [
+//   {
+//     id: 0,
+//     img: "img.png",
+//     name: "Kursnamn 1",
+//     details: "xx/xx/xx xx:xx",
+//     price: 100,
+//     spots: 10,
+//     info: "Info om kursen. Info om kursen. Info om kursen. Info om kursen. Info om kursen",
+//     duration: "xx:xx",
+//   },
+//   {
+//     id: 1,
+//     img: "img.png",
+//     name: "Kursnamn 2",
+//     details: "xx/xx/xx xx:xx",
+//     price: 300,
+//     spots: 10,
+//     info: "Info om kursen. Info om kursen. Info om kursen. Info om kursen. Info om kursen",
+//     duration: "xx:xx",
+//   },
+//   {
+//     id: 2,
+//     img: "img.png",
+//     name: "Kursnamn 3",
+//     details: "xx/xx/xx xx:xx",
+//     price: 600,
+//     spots: 10,
+//     info: "Info om kursen. Info om kursen. Info om kursen. Info om kursen. Info om kursen",
+//     duration: "xx:xx",
+//   }
+// ];
 
 function Courses() {
+
+  const [courses, setCourses] = useState([])
+  const coursesCollectionRef = collection(db, "courses");
+  
+  useEffect(() => {
+    const getCourses = async () => {
+      const data = await getDocs(coursesCollectionRef);
+      setCourses(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      };
+  
+    getCourses();
+  }, []);
+
+  
+
+
+  /* SKICKA TILL VARUKORG - onClick på kurskortens knapp
+    - 
+  
+
+
+  
+  */
+
+
   return (
     <main data-testid="courses" className={styles.wrapper}>
       <h2 className={styles.heading}>[kurser]</h2>
       <section className={styles.courseWrapper}>
-        {/* CourseItems ska sedan användas i .map från array  */}
-        <CourseItem props={testCourse} />
-        <CourseItem props={testCourse} />
-        <CourseItem props={testCourse} />
-        <CourseItem props={testCourse} />
+        {/* //map over the testcourses array and display it in the CourseItem component */}
+        {courses
+        .sort((course, nextCourse) => (course.details > nextCourse.details ? 1 : -1))
+        .map((course) => (<CourseItem key={course.id} courseData={course} />))}
       </section>
       
       <Events />
@@ -39,3 +89,4 @@ function Courses() {
 }
 
 export default Courses;
+
