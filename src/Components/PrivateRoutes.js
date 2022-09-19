33845f5bb4098029635433d9e01463fb46824f4a
Daniel Häggström <danielhaggstrom@hotmail.se>
2022-09-19 15:55:15 +0200

    progress

--- src/Components/PrivateRoutes.js
+++ src/Components/PrivateRoutes.js
@@ -1,50 +1,45 @@
-import { Outlet, Navigate,  } from 'react-router-dom';
-import { auth, db } from '../firebase-config'
+import { Outlet, Navigate } from "react-router-dom";
+import { auth, db } from "../firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
-import { useState, useEffect } from 'react';
-import { collection, doc, getDocs } from 'firebase/firestore'
-
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
 
 const PrivateRoutes = () => {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
   const [uid, setUid] = useState("");
-  const [newUsers, setNewUsers] = useState([]); 
-
+  const [users, setUsers] = useState([]);
   const [permission, setPermission] = useState(false);
   const usersCollectionRef = collection(db, "users");
 
+  useEffect(() => {
+    getUsers();
+    checkUser();
+    checkForAdmin();
+  }, []);
 
-  useEffect(() => {  
+  async function getUsers() {
+    const usersArr = await getDocs(usersCollectionRef);
+    setUsers(usersArr.docs.map((doc) => ({ ...doc.data() })));
+  }
+
+  function checkUser() {
     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
-      setUid(currentUser.uid)
-      
+      setUid(currentUser.uid);
     });
-  }, []);
+  }
 
-  useEffect(() => {
-    newUsers.forEach(user => { 
-      console.log("kör vi detta? test")
-      if(user.uid === uid) setPermission(user.admin)
-      console.log(user.admin)
-  })
-  }, [])
+  function checkForAdmin() {
+    const currUser = users.find((user) => user.uid === uid);
+    console.log(currUser);
+  }
 
-  useEffect(() => {
-    async function getUser(){
-      let vadsomhelst = await getDocs(usersCollectionRef)
-      setNewUsers(vadsomhelst.docs.map(doc => ({...doc.data(), id: doc.id})))
-    }
-    getUser()
-    
-  }, []);
-
-    
-
-    return (
-      <p>Detta är din nivå: {permission.toString()}</p>
-        //permission ? <Outlet /> : <Navigate to='/login' />
-    )
-}
+  return (
+    <h1 style={{ paddingTop: "150px" }}>
+      Detta är din nivå: {permission.toString()}
+    </h1>
+    //permission ? <Outlet /> : <Navigate to='/login' />
+  );
+};
 
 export default PrivateRoutes;
