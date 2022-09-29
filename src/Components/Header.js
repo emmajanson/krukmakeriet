84752c8e5d867cbf83c7433160455d95aae2008a
Fanny Reslow <reslowf@gmail.com>
2022-09-29 15:57:27 +0200

    fixing scroll to top

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import Basket from "./Basket";
@@ -8,7 +8,6 @@ import { onAuthStateChanged } from "firebase/auth";
 // fiUser ska sedan användas som profillogga när man är inloggad
 import { FaShoppingBag, FaUserAlt, FaUser } from "react-icons/fa";
 import HamburgerButton from "./HamburgerButton";
-import { act } from "react-test-renderer";
 // import { doc } from "firebase/firestore";
 
 function Header() {
@@ -17,14 +16,19 @@ function Header() {
   const [user, setUser] = useState({});
   const [isAdmin, setIsAdmin] = useState(false);
 
-  const { productBasket, courseBasket, adminPermission } =
-    useContext(AllContext);
+  const articleRef = useRef();
+
+  const {
+    productBasket,
+    courseBasket,
+    adminPermission,
+    showTopBtn,
+    setShowTopBtn,
+  } = useContext(AllContext);
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
-      act(() => {
-        setUser(currentUser);
-      });
+      setUser(currentUser);
     });
   }, []);
 
@@ -52,6 +56,22 @@ function Header() {
     setIsActiveBasket(props);
   }
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, [setShowTopBtn]);
+  function ScrollToView() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <header className={styles.wrapper}>
       <header className={styles.desktopWrapper}>
@@ -64,11 +84,15 @@ function Header() {
         </div>
 
         <nav className={styles.navMiddle}>
-          <Link title="kurser" to="/">
+          <Link title="kurser" to="/" onClick={ScrollToView}>
             Hem
           </Link>
-          <Link to="/courses">Kurser</Link>
-          <Link to="/shop">Butik</Link>
+          <Link to="/courses" onClick={ScrollToView}>
+            Kurser
+          </Link>
+          <Link to="/shop" onClick={ScrollToView}>
+            Butik
+          </Link>
         </nav>
 
         <nav className={styles.navRightSide}>
@@ -140,9 +164,15 @@ function Header() {
           }
         >
           <div className={styles.mobileMenuLinkWrapper}>
-            <Link to="/">Hem</Link>
-            <Link to="/courses">Kurser</Link>
-            <Link to="/shop">Butik</Link>
+            <Link to="/" name="home" onClick={ScrollToView}>
+              Hem
+            </Link>
+            <Link to="/courses" name="kurser" onClick={ScrollToView}>
+              Kurser
+            </Link>
+            <Link to="/shop" name="shop" onClick={ScrollToView}>
+              Butik
+            </Link>
             {user == null ? (
               <Link to="/signin">Logga in</Link>
             ) : (
