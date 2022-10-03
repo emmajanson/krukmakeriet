b464b04eb9f0b6607bba1149b33c07e3837400cf
Fanny Reslow <reslowf@gmail.com>
2022-10-03 10:06:12 +0200

    pushing up tests

--- src/Components/Shopitem.test.js
+++ src/Components/Shopitem.test.js
@@ -1,34 +1,18 @@
 import ShopItem from "./ShopItem";
-import {
-  render,
-  screen,
-  cleanup,
-  getByText,
-  getByTitle,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { AllContextProvider } from "../context/AllContext";
 import { BrowserRouter } from "react-router-dom";
 
-const mockProductData = [
-  {
-    name: "Testitem",
-    id: "test123",
-    img: "https://en.wikipedia.org/wiki/Pottery#/media/File:Potter_making_Pottery.jpg",
-    price: 100,
-  },
-  {
-    description: "second item",
-    completed: false,
-  },
-  {
-    description: "third item",
-    completed: false,
-  },
-];
+const mockProductData = {
+  name: "Testitem",
+  id: "test123",
+  img: "https://en.wikipedia.org/wiki/Pottery#/media/File:Potter_making_Pottery.jpg",
+  price: 100,
+};
 
 describe("ShopItem", () => {
-  test("check if props exist as should", async () => {
+  test("check if props exist and are rendered", async () => {
     render(
       <>
         <BrowserRouter>
@@ -40,6 +24,10 @@ describe("ShopItem", () => {
     );
 
     let itemEle = screen.getByTitle("productItem");
+    let nameExist = screen.getByText(/Testitem/i);
+
     expect(itemEle).toBeInTheDocument();
+    expect(nameExist).toBeInTheDocument();
+    expect(nameExist).toHaveTextContent(/Testitem/i);
   });
 });
