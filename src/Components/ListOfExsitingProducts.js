8fca291ff6fad55fd2546bfc05ec4bca218de6ca
Ann-Helen Lausmaa <code@ahlvdesign.se>
2022-10-03 09:35:28 +0200

    admin styling

--- src/Components/ListOfExsitingProducts.js
+++ src/Components/ListOfExsitingProducts.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { db } from "../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 import styles from "./ListOfExistingProducts.module.css";
-import { FaCaretRight } from "react-icons/fa";
+import { FaChevronRight, FaPlus } from "react-icons/fa";
 import UpdateProducts from "./UpdateProducts";
 
 function ListOfExsitingProducts() {
@@ -60,7 +60,10 @@ function ListOfExsitingProducts() {
       <h3 className={styles.title}>Butik</h3>
       <p className={styles.text}>Lägg till ny produkt eller välj befintlig för att uppdatera</p>
         <button className={styles.button} onClick={() => toggleNewProduct()}>
-          Lägg till ny produkt +
+          <p className={styles.btnText}>Lägg till ny kurs</p>
+          <div className={styles.iconWrapper}>
+            <FaPlus className={styles.plusIcon}/>
+          </div>
         </button>
         {products.map((product, index) => {
           return (
@@ -84,7 +87,7 @@ function ListOfExsitingProducts() {
               <div className={styles.nameWrapper}>
                 <p className={styles.name}>{product.name}</p>
               </div>
-              <FaCaretRight className={styles.FaCaretRight} />
+              <FaChevronRight className={styles.FaCaretRight} />
             </div>
           );
         })}
